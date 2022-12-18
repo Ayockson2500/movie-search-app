7f7689c24df42161c4f2ad62d65524fa7625534e
ayockson2500 <ayockson2500@gmail.com>
2022-12-18 05:41:11 +0100

    new changes made

---
+++ src/StyledApp.styles.jsx
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+import headerImage from "./assets/images/movie-header-Img.svg"
+
+const StyledApp = styled.div`
+@import url('https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;700&display=swap');
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: "DM Sans";
+    }
+    .f-s-72 {
+        font-size: 72;
+    }
+    .f-w-700 {
+        font-weight: 700;
+        font-family: "DM Sans";
+    }
+
+    nav {
+        background: #292929;
+        color: white;
+        padding: 10px;
+    }
+    .app-logo {
+        border: 1px solid white;
+        width: 100px;
+        padding: 8px;
+    }
+    header {
+        background: url(${headerImage});
+        height: 50vh;
+        width: 100%;
+    }
+    .header-title {
+        width: 200px;
+        position: absolute;
+        top: 150px;
+        border: 1px solid white;
+    }
+    header p {
+        color:#FFFFFF;
+    }
+    .searchField {
+        padding: 10px;
+    }
+    .searchField input {
+        width: 100%;
+    }
+    .movie-app > .row {
+        overflow-x: auto;
+        flex-wrap: nowrap;
+    }
+`
+export default StyledApp
